92eb57c1870266667324f9ee08ace93265b16eaa
Alex Morega <alex@grep.ro>
2011-12-13 11:26:47 +0200

    bring in Frozen-Flask

--- lista.py
+++ lista.py
@@ -1,11 +1,15 @@
 import flask
+import flask_frozen
 
 app = flask.Flask(__name__)
+freezer = flask_frozen.Freezer(app)
 
 
 @app.route('/')
 def homepage():
     return 'hello world'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.freeze()
+    freezer.serve()
