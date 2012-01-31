e36104979e6c130c23c5ead2c8384b0dd2dffa9f
Alex Morega <alex@grep.ro>
2012-02-01 00:11:05 +0200

    load list of articles

--- lista.py
+++ lista.py
@@ -1,8 +1,21 @@
+import py.path
 import flask
 import flask_frozen
 
 app = flask.Flask(__name__)
 freezer = flask_frozen.Freezer(app)
+
+
+def load_articles(articles_dir_path):
+    articles = {}
+    for article_path in articles_dir_path.listdir():
+        article_id = article_path.basename.rsplit('.', 1)[0]
+        with article_path.open('rb') as f:
+            articles[article_id] = f.read()
+    return articles
+
+
+articles = load_articles(py.path.local(__file__).dirpath().join('articles'))
 
 
 @app.route('/')
