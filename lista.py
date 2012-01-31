f9d5dda6a4a02c63d680b65203caa2c5da49bd70
Alex Morega <alex@grep.ro>
2012-02-01 00:28:32 +0200

    read article metadata as yaml

--- lista.py
+++ lista.py
@@ -1,4 +1,5 @@
 import py.path
+import yaml
 import flask
 import flask_frozen
 
@@ -7,11 +8,20 @@ freezer = flask_frozen.Freezer(app)
 
 
 def load_articles(articles_dir_path):
-    articles = {}
+    articles = []
     for article_path in articles_dir_path.listdir():
         article_id = article_path.basename.rsplit('.', 1)[0]
         with article_path.open('rb') as f:
-            articles[article_id] = f.read()
+            header = ""
+            lines = (l.decode('utf-8') for l in f)
+            for line in lines:
+                if line.strip() == '---':
+                    break
+                header += line
+            content = ''.join(lines)
+            articles.append(dict(yaml.load(header),
+                                 id=article_id,
+                                 content=content))
     return articles
 
 
