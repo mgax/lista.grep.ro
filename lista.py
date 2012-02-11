31533ca5461448e4447f09a30f756186936fa813
Alex Morega <alex@grep.ro>
2012-02-11 18:54:43 +0200

    events are yaml files; no html contents

--- lista.py
+++ lista.py
@@ -12,16 +12,9 @@ def load_events(events_dir_path):
     for event_path in events_dir_path.listdir():
         event_id = event_path.basename.rsplit('.', 1)[0]
         with event_path.open('rb') as f:
-            header = ""
-            lines = (l.decode('utf-8') for l in f)
-            for line in lines:
-                if line.strip() == '---':
-                    break
-                header += line
-            content = ''.join(lines)
-            events.append(dict(yaml.load(header),
-                                 id=event_id,
-                                 content=content))
+            event_data = yaml.load(f)
+            event_data['id'] = event_id
+            events.append(event_data)
     return events
 
 
