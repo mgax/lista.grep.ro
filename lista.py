5cac0d8fa45be48aa1e86484f3a8b8bd6d878d46
Alex Morega <alex@grep.ro>
2012-02-26 20:04:16 +0200

    atom feed

--- lista.py
+++ lista.py
@@ -1,9 +1,12 @@
 import operator
+import urlparse
 import py.path
 import yaml
 import flask
 import flask_frozen
 import flaskext.script
+from werkzeug.contrib.atom import AtomFeed
+
 
 app = flask.Flask(__name__, instance_relative_config=True)
 freezer = flask_frozen.Freezer(app)
@@ -18,6 +21,7 @@ def load_events(events_dir_path):
         with event_path.open('rb') as f:
             event_data = yaml.load(f)
             event_data['id'] = event_id
+            event_data.setdefault('change-date', event_data['post-date'])
             events.append(event_data)
     events.sort(key=operator.itemgetter('date'), reverse=True)
     return events
@@ -30,6 +34,23 @@ events = load_events(events_folder)
 @app.route('/')
 def homepage():
     return flask.render_template('index.html', events=events)
+
+
+@app.route('/recent.atom')
+def recent_feed():
+    site_url = 'http://lista.grep.ro/'
+    feed = AtomFeed("Lista hackerului social",
+                    feed_url=urlparse.urljoin(site_url, flask.request.path),
+                    url=site_url)
+
+    ordered_events = sorted(events, key=operator.itemgetter('change-date'))
+    for event in reversed(ordered_events[-20:]):
+        feed.add(event['title'],
+                 url=event['url'],
+                 updated=event['change-date'],
+                 published=event['post-date'],
+                 author="Alex Morega")
+    return feed.get_response()
 
 
 @manager.command
