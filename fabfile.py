80c41af9eae154673f5e3eb012fc6fe2e812cace
Alex Morega <alex@grep.ro>
2012-10-25 21:21:08 +0300

    don't require a local fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,13 @@
-from fabric.api import local
+from fabric.api import env, local
 from fabric.contrib.project import rsync_project
-from local_fabfile import *
+from fabric.api import env
+
+
+env['use_ssh_config'] = True
+
+env['hosts'] = ['redcoat']
+
+REMOTE_PATH = '/home/alexm/sites/lista.grep.ro'
 
 
 def deploy():
