6baa846eca841d783527c759fe982f4856eb83e3
Alex Morega <alex@grep.ro>
2012-11-01 11:51:15 +0200

    link to our own feed

--- settings.py
+++ settings.py
@@ -1,4 +1,3 @@
 GOOGLE_ANALYTICS_CODE = 'UA-29128629-1'
 FEEDBURNER_MAIL_URL = ('http://feedburner.google.com/fb/a/mailverify?'
                        'uri=ListaHackeruluiSocial')
-FEEDBURNER_FEED_URL = 'http://feeds.feedburner.com/ListaHackeruluiSocial'
